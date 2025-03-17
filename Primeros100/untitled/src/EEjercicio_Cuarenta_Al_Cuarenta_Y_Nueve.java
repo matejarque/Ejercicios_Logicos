37dc61d6a262ec071b4abf035b9b86898c475af1
Matias <ejarquegithub@gmail.com>
2025-03-16 22:54:31 -0300

    Ejercicio48
    * Crea una función que reciba dos cadenas como parámetro (str1, str2)
         * e imprima otras dos cadenas como salida (out1, out2).
         * - out1 contendrá todos los caracteres presentes en la str1 pero NO
         *   estén presentes en str2.
         * - out2 contendrá todos los caracteres presentes en la str2 pero NO
         *   estén presentes en str1.

--- Primeros100/untitled/src/EEjercicio_Cuarenta_Al_Cuarenta_Y_Nueve.java
+++ Primeros100/untitled/src/EEjercicio_Cuarenta_Al_Cuarenta_Y_Nueve.java
@@ -209,4 +209,42 @@ El sistema debe calcular el costo total de la estadia dependiendo si escogio un 
          return new String(charInvertido);
      }
 
+
+     /*
+
+     * Crea una función que reciba dos cadenas como parámetro (str1, str2)
+     * e imprima otras dos cadenas como salida (out1, out2).
+     * - out1 contendrá todos los caracteres presentes en la str1 pero NO
+     *   estén presentes en str2.
+     * - out2 contendrá todos los caracteres presentes en la str2 pero NO
+     *   estén presentes en str1.
+     */
+
+     private void cuarentaYOcho(String str1, String str2) {
+         StringBuilder out1 = new StringBuilder();
+         StringBuilder out2 = new StringBuilder();
+
+         for (char c : str1.toCharArray()) {
+             if (str2.indexOf(c) == -1) {
+                 out1.append(c);
+             }
+         }
+
+         for (char c : str2.toCharArray()) {
+             if (str1.indexOf(c) == -1) {
+                 out2.append(c);
+             }
+         }
+
+         System.out.println("out1: " + out1);
+         System.out.println("out2: " + out2);
+     }
+
+
+
+
+
+
+
+
 }
